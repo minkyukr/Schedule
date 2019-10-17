e1828dd0dc3380aeee3ff33fbfb6d9a64c2b42e8
Michel Kong <minkya_u@daum.net>
2019-10-17 14:02:23 -0700

    first row added

---
+++ Assets/script.js
@@ -0,0 +1,40 @@
+
+
+function settingTable (time) {
+  var targetDiv = document.querySelector('.container');
+  //selecting where to put
+  var newTable = document.createElement('tr');
+  //creating an element to put in container
+  newTable.setAttribute('id', 'tableRows')
+  //setting the attributes in order to add columns to it.
+  targetDiv.appendChild(newTable);
+  //appending it to container.
+
+  var targetTable = document.querySelector('#tableRows');
+  //select tableRows to add the follow columns.
+
+  var dateTd = document.createElement('td');
+  var inputTd = document.createElement('td');
+  var saveTd = document.createElement('td');
+  //creating tds
+
+  dateTd.textContent = time;
+  inputTd.textContent ='que horas?';
+  saveTd.textContent ='save button';
+
+  dateTd.setAttribute('class', 'hour');
+  inputTd.setAttribute('class', 'description');
+  //not sure if it is the correct class target from css.
+  saveTd.setAttribute('class', 'saveBtn');
+
+  //setting attributes for css later on
+
+  tableRows.appendChild(dateTd);
+  tableRows.appendChild(inputTd);
+  tableRows.appendChild(saveTd);
+  //appending everything to tableRows
+}
+
+settingTable(9)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Schedule
+
+1. Display todays date. Using moment.js.
+    a. Switch the colors when it has passed. Refer to CSS.
+
+    On the load, check if the hour has passed or not.
+    with the if statement, check for every single one.
+
+2. Create a table with the hours: 9 - 5. Three Columns and nine rows.
+    1st Column - Hours. 2nd Column user input. 3rd Save button.
+
+        1. Create a funtion so that it we called nine times.
+        2. Inside of function, create one row that contains three columns.
+            1. hour - should show the hour.
+            2. input - input area. (text - save the text to local storage)
+            3. save - save button. (img onclick save it to local storage)
+
+
