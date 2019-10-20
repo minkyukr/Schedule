5e5a336556682ec62ffc59b779436be142d4bf67
minkyukr <minkya_u@daum.net>
2019-10-19 21:52:38 -0700

    almost done but still not working

--- Assets/script.js
+++ Assets/script.js
@@ -5,9 +5,101 @@ todayDate.textContent = moment().format('dddd MMMM Do YYYY');
 targetDate.appendChild(todayDate);
 //this is where the today's date shows in the html.
 
-var hour = moment().format('HH')
-var number = 9
+// var hour = moment().format('HH')
+// var number = 9
 // if (number < 10) {
 //   document.getElementsByClassName('col-md-10').style.backgroundColor = 'green';
 // }
+function loadPage() {
+  document.querySelector('.saveBtn').addEventListener('click', function () {
+    var userInput = document.querySelector('.description').value;
+    var time = $(this).parent().attr("id");
+    //이거는 모르겟다..
 
+    localStorage.setItem(time, userInput);
+  });
+
+  function hourUpdate() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  } 
+  hourUpdate();
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+}
+
+// $(document).ready(function() {
+//     // listen for save button clicks
+//     $(".saveBtn").on("click", function() {
+//       // get nearby values
+//       var value = $(this).siblings(".description").val();
+//       var time = $(this).parent().attr("id");
+  
+//       // save in localStorage
+//       localStorage.setItem(time, value);
+//     });
+  
+//     function hourUpdater() {
+      // get current number of hours
+      // var currentHour = moment().hours();
+  
+      // // loop over time blocks
+      // $(".time-block").each(function() {
+      //   var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+      //   // check if we've moved past this time
+      //   if (blockHour < currentHour) {
+      //     $(this).addClass("past");
+      //   } 
+      //   else if (blockHour === currentHour) {
+      //     $(this).removeClass("past");
+      //     $(this).addClass("present");
+      //   } 
+      //   else {
+      //     $(this).removeClass("past");
+      //     $(this).removeClass("present");
+      //     $(this).addClass("future");
+      //   }
+      // });
+//     }
+  
+//     hourUpdater();
+  
+//     // set up interval to check if current time needs to be updated
+//     var interval = setInterval(hourUpdater, 15000);
+  
+//     // load any saved data from localStorage
+//     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+//     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+//     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+//     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+//     $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+//     $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+//     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+//     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+//     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+// }
